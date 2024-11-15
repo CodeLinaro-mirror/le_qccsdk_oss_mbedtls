551c8674c7c5471f6e9fc01caa4dfce26840a0ad
Bolin Wang <quic_boliwang@quicinc.com>
2024-11-15 10:22:17 +0800

    Fix the copyright issue of MbedTLS
    Add License selection
    Move Qualcomm copyright after the original one
    
    Change-Id: Id90590aacb66a893040ff8b52a512dbe19664677
    Signed-off-by: Bolin Wang <quic_boliwang@quicinc.com>

--- port/rsa_alt.c
+++ port/rsa_alt.c
@@ -1,13 +1,17 @@
 /*
- * Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
- * SPDX-License-Identifier: BSD-3-Clause-Clear
- */
-
-/*
  *  The RSA public-key cryptosystem
  *
  *  Copyright The Mbed TLS Contributors
  *  SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
+ *
+ *  Qualcomm Innovation Center, Inc. chooses to take subject only to the terms of the Apache-2.0 license.
+ */
+
+/*
+ * Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ *
+ * NOT A CONTRIBUTION
  */
 
 /*
